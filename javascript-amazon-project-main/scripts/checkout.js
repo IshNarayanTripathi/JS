ccbc3e7aa48876dd2b7f46263c97925b3bfb2474
IshNarayanTripathi <ishu012346@icloud.com>
2025-11-27 00:40:20 +0530

    Add formatMoney utility function and update price formatting in product and checkout displays

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatMoney} from "./utils/money.js";
   
 let cartHTML = "";
 
@@ -20,7 +21,7 @@ cart.forEach((cartItem) => {
                   ${product.name}
                 </div>
                 <div class="product-price">
-                  $${(product.priceCents / 100).toFixed(2)}
+                  $${formatMoney(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -42,7 +43,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -55,7 +56,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -68,7 +69,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
